f96b89491b34b8e5a12cc2a4c6e04d42ec0f2c15
JaC1227 <156141013+JaC1227@users.noreply.github.com>
2024-12-12 15:21:14 -0500

    Update contact.js

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -1,41 +1,32 @@
-const nodemailer = require('nodemailer');
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+const mailgun = new Mailgun(formData);
 require('dotenv').config();
 
 exports.handler = async (event) => {
   try {
-    // Parse the incoming form data
     const { name, phone, email, subject, message } = JSON.parse(event.body);
-
-    // Create the transporter using Mailgun's SMTP server
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.mailgun.org',
-      port: 587,
-      auth: {
-        user: process.env.MAILGUN_DOMAIN,
-        pass: process.env.MAILGUN_API_KEY,
-      },
-    });
-
-    // Email options
-    const mailOptions = {
-      from: `postmaster@sandbox3cda22694a9d47afb5ef2fda3a82243d.mailgun.org`, // Your Mailgun domain's "postmaster" email
-      to: 'jaedynchinn@icloud.com', // Recipient email
-      subject: subject, // Form subject
-      text: `Message from: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`, // Body of the email
+    
+    const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY});
+    
+    const data = {
+      from: "contact form <mailgun@sandbox3cda22694a9d47afb5ef2fda3a82243d.mailgun.org>",
+      to: ["jaedynchinn@icloud.com"],
+      subject: subject,
+      text: `Message from: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`,
+      html: `<h1>Message from: ${name}</h1><p>${message}</p>`
     };
-
-    // Send the email
-    await transporter.sendMail(mailOptions);
+    
+    const response = await mg.messages.create('sandbox3cda22694a9d47afb5ef2fda3a82243d.mailgun.org', data);
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ success: true }),
+      body: JSON.stringify({ success: true, message: response }),
     };
   } catch (error) {
-    console.error(error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Failed to send message" }),
+      body: JSON.stringify({ error: "Failed to send message", details: error }),
     };
   }
 };
