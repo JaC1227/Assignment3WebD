cc7a818cd4f8104f5ba2eff78ffde0de32aafda2
JaC1227 <156141013+JaC1227@users.noreply.github.com>
2025-01-20 21:37:54 -0500

    removed prof email

--- .netlify/functions/send-email.js
+++ .netlify/functions/send-email.js
@@ -24,7 +24,7 @@ exports.handler = async (event) => {
     // Prepare email data
     const data = {
       from: `"${name}" <mailgun@sandbox3cda22694a9d47afb5ef2fda3a82243d.mailgun.org>`,
-      to: ["jaedynchinn@icloud.com", "adam.kunz@durhamcollege.ca"], 
+      to: ["jaedynchinn@icloud.com"], 
       subject: `[This message was sent automatically] ${subject}`,
       text: `Message: ${message}\nPhone: ${phone}`,
       html: `<h4>${message}</h4><p>${phone}</p>`,
